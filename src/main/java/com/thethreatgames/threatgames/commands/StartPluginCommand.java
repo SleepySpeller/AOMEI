e44c39ccf32de75c34cb98da4949f53882fe8504
SleepySpeller <support@slpy.ml>
2023-02-14 20:05:52 +0100

    Fixed a bug with running the code in the console

--- src/main/java/com/thethreatgames/threatgames/commands/StartPluginCommand.java
+++ src/main/java/com/thethreatgames/threatgames/commands/StartPluginCommand.java
@@ -9,35 +9,33 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class StartPluginCommand implements CommandExecutor {
-    private final Threatgames main;
-
-    public StartPluginCommand(Threatgames main){
-        this.main = main;
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = ((Player) sender).getPlayer();
-
-        if(!sender.hasPermission("aomei.toggle") || !sender.isOp()){
-            sender.sendMessage(ChatColor.RED + "You don't have the permission to execute this command");
-            return true;
-        }
-        if(ConfigManager.playerscfg.getBoolean("enabled") == false){
-            ConfigManager.playerscfg.set("enabled", true);
-            ConfigManager.savePlayers();
-            ConfigManager.reloadPlayers();
-            player.sendMessage(ChatColor.GREEN + "The AOMEI Plugin has been enabled!");
-            return true;
-        }else{
-            ConfigManager.playerscfg.set("enabled", false);
-            ConfigManager.savePlayers();
-            ConfigManager.reloadPlayers();
-            player.sendMessage(ChatColor.DARK_AQUA + "The AOMEI Plugin has been disabled!");
-            return true;
-        }
-
-
-
-    }
+  private final Threatgames main;
+  
+  public StartPluginCommand(Threatgames main) {
+    this.main = main;
+  }
+  
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (!(sender instanceof Player)) {
+      sender.sendMessage(ChatColor.RED + "You have to be a player to execute this command");
+      return true;
+    } 
+    Player player = ((Player)sender).getPlayer();
+    if (!sender.hasPermission("aomei.toggle") || !sender.isOp()) {
+      sender.sendMessage(ChatColor.RED + "You don't have the permission to execute this command");
+      return true;
+    } 
+    if (!ConfigManager.playerscfg.getBoolean("enabled")) {
+      ConfigManager.playerscfg.set("enabled", Boolean.valueOf(true));
+      ConfigManager.savePlayers();
+      ConfigManager.reloadPlayers();
+      player.sendMessage(ChatColor.GREEN + "The AOMEI Plugin has been enabled!");
+      return true;
+    } 
+    ConfigManager.playerscfg.set("enabled", Boolean.valueOf(false));
+    ConfigManager.savePlayers();
+    ConfigManager.reloadPlayers();
+    player.sendMessage(ChatColor.DARK_AQUA + "The AOMEI Plugin has been disabled!");
+    return true;
+  }
 }
